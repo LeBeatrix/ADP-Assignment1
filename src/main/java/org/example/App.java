e679071a6ac98ad67dec20799f72e58c346647ce
LeBeatrix <47351662+LeBeatrix@users.noreply.github.com>
2020-02-18 22:49:59 +0200

    Initial Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        //add some comments Bruh!
         System.out.println( "This is my first assignment" );
+
+        //Cloned by Lene
+        System.out.println( "Okay Cool!" );
     }
 }
